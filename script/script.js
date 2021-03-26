40480c2013ced856494106ffedc31e5059465608
n-desantis92 <dsnwebdev@gmail.com>
2021-03-26 16:41:04 +0100

    aggiunto remove e add item

--- script/script.js
+++ script/script.js
@@ -2,12 +2,22 @@ var app = new Vue(
   {
     el: "#todolist",
     data: {
+      newItem: "",
       lista: [
         "cocomero",
         "cipolla",
         "sesamo",
         "farina"
       ],
-      
+
+    },
+    methods: {
+      deletItem: function(itemIndex) {
+        this.lista.splice(itemIndex, 1);
+      },
+      addElement: function() {
+        this.lista.push(this.newItem);
+        this.newItem = "";
+      }
     }
 });
